7da8e9c8f449f8f0968b7060695fcd8ddb5d7577
Your Name <lokeshtiwari212@gmail.com>
2024-03-14 16:14:06 +0530

    update hero

--- my-portfolio/src/Components/Hero.js
+++ my-portfolio/src/Components/Hero.js
@@ -1,19 +1,28 @@
 import React from "react";
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from "react-bootstrap";
 
 function Hero() {
   return (
     <div>
-      <Container>
+      <Container className="pb-5 pt-5">
         <Row>
-          <Col md={6}>
-            <p><i>Hello!!!</i></p>
-            <h3>I'm Lokesh Tiwari</h3>
-<h4>Iam a Web Developer</h4>
-<p>Greetings, I am Lokesh Tiwari, an accomplished Full Stack Developer currently serving at Godigitify Nexus. Possessing over a 1+ year of industry experience, I am fervently dedicated to harnessing web technologies to craft captivating digital experiences. My proficiency extends across the entire spectrum of web development, encompassing both front-end and back-end domains.</p>
+          <Col md={6} className="p-3">
+            <p className="hero-para">
+              <i>Hello!!!</i>
+            </p>
+            <h3 className="hero-h3">I'm Lokesh Tiwari</h3>
+            <h4 hero-h4>Iam a Web Developer</h4>
+            <p hero-content>
+              Greetings, I am Lokesh Tiwari, an accomplished Full Stack
+              Developer currently serving at Godigitify Nexus. Possessing over a
+              1+ year of industry experience, I am fervently dedicated to
+              harnessing web technologies to craft captivating digital
+              experiences. My proficiency extends across the entire spectrum of
+              web development, encompassing both front-end and back-end domains.
+            </p>
           </Col>
 
-          <Col md={6}></Col>
+          <Col md={6} className="p-3"></Col>
         </Row>
       </Container>
     </div>
