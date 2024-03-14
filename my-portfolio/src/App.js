d169c9e50924bad9586ac4b3140d4fcde2521580
Your Name <lokeshtiwari212@gmail.com>
2024-03-14 14:30:52 +0530

    statusbar

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Statusbar from './Layout/Statusbar'
+function App() {
+  return (
+   <>
+   <Statusbar></Statusbar>
+   </>
+  );
+}
+
+export default App;
