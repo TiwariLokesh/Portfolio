7df007b70a07c4826f9133fa9997613280997124
Your Name <lokeshtiwari212@gmail.com>
2024-03-14 16:27:14 +0530

    update

--- my-portfolio/src/Layout/Statusbar.js
+++ my-portfolio/src/Layout/Statusbar.js
@@ -4,7 +4,7 @@ import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import '../Stylings/Home.css'
+import '../Layout/Statusbar.css'
 
 function NavScrollExample() {
   return (
