206d9810785886a34625b4612f398b7c36173b41
Your Name <lokeshtiwari212@gmail.com>
2024-03-14 15:21:40 +0530

    update

--- my-portfolio/src/Layout/Statusbar.js
+++ my-portfolio/src/Layout/Statusbar.js
@@ -5,6 +5,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
+
 function NavScrollExample() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary shadow py-3 px-5">
