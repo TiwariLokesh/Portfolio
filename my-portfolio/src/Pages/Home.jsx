76f7bd9c5074e2fe00580c64bdfcdaff0590f2bd
Your Name <lokeshtiwari212@gmail.com>
2024-03-15 23:52:59 +0530

    update

--- my-portfolio/src/Pages/Home.jsx
+++ my-portfolio/src/Pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Statusbar from '../Layout/Statusbar'
 import Hero from '../Components/Hero'
-
+import Projects from '../Components/Projects'
 function Home() {
   return (
     <>
     <Statusbar></Statusbar>
     <Hero></Hero>
+    <Projects></Projects>
     </>
   )
 }
