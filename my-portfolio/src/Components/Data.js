37701397789bd9802b79d6897799d04e647c20ed
Your Name <lokeshtiwari212@gmail.com>
2024-03-16 21:16:30 +0530

    update

--- my-portfolio/src/Components/Data.js
+++ my-portfolio/src/Components/Data.js
@@ -1,5 +1,8 @@
 const productsData = [
     {
-        
+image : 'https://img.freepik.com/premium-photo/computer-screen-with-colorful-paint-it-generative-ai_97167-1066.jpg',
+title: 'Graphic',
+disc : "lorem ipsum",
+link : 
     }
 ]
