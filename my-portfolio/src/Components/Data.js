31ec07be77884aeb4990dbe5a24455e3971503a0
Your Name <lokeshtiwari212@gmail.com>
2024-03-16 21:17:14 +0530

    update

--- my-portfolio/src/Components/Data.js
+++ my-portfolio/src/Components/Data.js
@@ -1,8 +1,12 @@
 const productsData = [
-    {
-image : 'https://img.freepik.com/premium-photo/computer-screen-with-colorful-paint-it-generative-ai_97167-1066.jpg',
-title: 'Graphic',
-disc : "lorem ipsum",
-link : 
-    }
-]
+  {
+    image: "https://img.freepik.com/premium-photo/computer-screen-with-colorful-paint-it-generative-ai_97167-1066.jpg",
+    title: "Graphic",
+    disc: "lorem ipsum",
+  },
+  {
+    image: "https://img.freepik.com/premium-photo/computer-screen-with-colorful-paint-it-generative-ai_97167-1066.jpg",
+    title: "Web",
+    disc: "lorem ipsum",
+  },
+];
