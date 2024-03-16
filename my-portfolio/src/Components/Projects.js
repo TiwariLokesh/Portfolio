f0c5fd1ca3e01a2e2343cd968f9e8a7e0d8091a6
Your Name <lokeshtiwari212@gmail.com>
2024-03-16 22:30:20 +0530

    update

--- my-portfolio/src/Components/Projects.js
+++ my-portfolio/src/Components/Projects.js
@@ -5,23 +5,23 @@ import Card from "react-bootstrap/Card";
 
 function Projects(props) {
   return (
-    <Container>
+    <>
       <Row className="special">My Projects</Row>
-      <Row className="align-items-center"> 
-        <Col lg={4} md={6}>
-          <Card style={{ width: "25rem" }}>
-            <Card.Img variant="top" src={props.data.image} />
-            <Card.Body>
-              <Card.Title>{props.data.title}</Card.Title>
-              <Card.Text>
-                {props.data.disc}
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+      <Container>
+        <Row className="align-items-center">
+          <Col lg={4} md={6}>
+            <Card style={{ width: "25rem" }}>
+              <Card.Img variant="top" src={props.data.image} />
+              <Card.Body>
+                <Card.Title>{props.data.title}</Card.Title>
+                <Card.Text>{props.data.disc}</Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
