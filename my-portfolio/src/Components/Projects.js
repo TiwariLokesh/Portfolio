187ba534eaed00edecc067f4c0496ef904629734
Your Name <lokeshtiwari212@gmail.com>
2024-03-17 14:08:23 +0530

    update

--- my-portfolio/src/Components/Projects.js
+++ my-portfolio/src/Components/Projects.js
@@ -3,7 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
-function Projects(props) {
+function Projects() {
   return (
     
      <div>
--- my-portfolio/src/Pages/Home.js
+++ my-portfolio/src/Pages/Home.js
@@ -2,8 +2,8 @@ import React from "react";
 import Statusbar from "../Layout/Statusbar";
 import Hero from "../Components/Hero";
 import Projects from "../Components/Projects";
-import Data from "../Components/Data";
-import { Container, Row, Col } from "react-bootstrap";
+
+
 
 function Home() {
   return (
