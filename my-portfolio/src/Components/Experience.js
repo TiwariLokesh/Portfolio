d7707ab2638040e5b300da03a4a68ad4a69a23f5
Your Name <lokeshtiwari212@gmail.com>
2024-03-17 16:25:04 +0530

    update

--- my-portfolio/src/Components/Experience.js
+++ my-portfolio/src/Components/Experience.js
@@ -3,8 +3,11 @@ import { Container ,Row } from 'react-bootstrap'
 
 function Experience() {
   return (
+    
     <Container className='pb-5'>
-        
+        <Row className='pb-5'>
+        <div className="text-center project-heading pb-5">My <span>Experience</span></div>   
+        </Row>
         <Row className='justify-content-center align align-items-center pb-5'>
     <div className="box d-flex justify-content-center align-items-center">
     <ul>
