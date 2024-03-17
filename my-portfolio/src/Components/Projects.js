74ed651164a710ed2090572722d9604e55aebc4f
Your Name <lokeshtiwari212@gmail.com>
2024-03-17 14:58:42 +0530

    update

--- my-portfolio/src/Components/Projects.js
+++ my-portfolio/src/Components/Projects.js
@@ -13,9 +13,20 @@ function Projects() {
         <Row className="align-items-center">
           <Col lg={4} md={6} className="d-flex justify-content-center align-content-center">
             <Card className="cards"style={{ width: "24rem", }}>
-              <Card.Img variant="top" src="https://img.freepik.com/premium-photo/computer-screen-with-colorful-paint-it-generative-ai_97167-1066.jpg" />
+              <Card.Img variant="top" src="https://img.freepik.com/free-vector/web-development-programmer-engineering-coding-website-augmented-reality-interface-screens-developer-project-engineer-programming-software-application-design-cartoon-illustration_107791-3863.jpg?w=1060&t=st=1710667406~exp=1710668006~hmac=5cc28ee3a3f3dbe056a2ce35c33e2042045e2f1659029c4aead4457c0769bf1d" />
               <Card.Body>
                 <p><span className="card-title">Tech Connect</span></p>
+                <Card.Text className="card-content ">A Social Media app for developers by the developers. Geeks can post topics on new technologies and follow geeks with same interest.</Card.Text>
+                <Button variant=" project-button">Read More</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col lg={4} md={6} className="d-flex justify-content-center align-content-center">
+            <Card className="cards" style={{ width: "24rem" }}>
+              <Card.Img variant="top" src="https://img.freepik.com/premium-photo/computer-screen-with-colorful-paint-it-generative-ai_97167-1066.jpg" />
+              <Card.Body>
+              <p><span className="card-title">Crypto Tracker</span></p>
                 <Card.Text>lorem ipsum</Card.Text>
                 <Button variant="primary">Go somewhere</Button>
               </Card.Body>
@@ -26,18 +37,7 @@ function Projects() {
             <Card className="cards" style={{ width: "24rem" }}>
               <Card.Img variant="top" src="https://img.freepik.com/premium-photo/computer-screen-with-colorful-paint-it-generative-ai_97167-1066.jpg" />
               <Card.Body>
-              <p><span className="card-title">Tech Connect</span></p>
-                <Card.Text>lorem ipsum</Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-
-          <Col lg={4} md={6} className="d-flex justify-content-center align-content-center">
-            <Card className="cards" style={{ width: "24rem" }}>
-              <Card.Img variant="top" src="https://img.freepik.com/premium-photo/computer-screen-with-colorful-paint-it-generative-ai_97167-1066.jpg" />
-              <Card.Body>
-              <p><span className="card-title">Tech Connect</span></p>
+              <p><span className="card-title">KK. Electric (Web)</span></p>
                 <Card.Text>lorem ipsum</Card.Text>
                 <Button variant="primary">Go somewhere</Button>
               </Card.Body>
