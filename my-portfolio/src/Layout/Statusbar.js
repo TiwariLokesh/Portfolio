1ded44e3ceac2bb5db012d9f08e6cb92f6c1b7f3
Your Name <lokeshtiwari212@gmail.com>
2024-03-20 16:24:36 +0530

    updated

--- my-portfolio/src/Layout/Statusbar.js
+++ my-portfolio/src/Layout/Statusbar.js
@@ -48,7 +48,7 @@ function Statusbar() {
   return (
     <>
       {[false].map((expand) => (
-        <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3 px-3">
+        <Navbar sticky="top" key={expand} expand={expand} className="mb-3 px-3">
           <Container fluid>
             <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
@@ -69,6 +69,7 @@ function Statusbar() {
            <Nav.Link href="#action2">My Blogs</Nav.Link>
             <Nav.Link href="#action2">My Workshop</Nav.Link>
                   </Nav>
+                  <Button variant=" button-color my-1">Contact</Button>
               </Offcanvas.Body>
             </Navbar.Offcanvas>
           </Container>
