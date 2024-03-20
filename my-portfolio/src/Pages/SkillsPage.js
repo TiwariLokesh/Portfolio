90fa7b70c9f073ebb234ae1bbb09ac856c8aaa2c
Your Name <lokeshtiwari212@gmail.com>
2024-03-20 18:40:51 +0530

    made SkillsPage page

---
+++ my-portfolio/src/Pages/SkillsPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Statusbar from '../Layout/Statusbar'
+function SkillsPage() {
+  return (
+    <div>
+        <Statusbar></Statusbar>
+    </div>
+  )
+}
+
+export default SkillsPage
