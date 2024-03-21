b5dd0ee4224c9eeffccf73220ffbac1ea3e16713
Your Name <lokeshtiwari212@gmail.com>
2024-03-21 15:04:16 +0530

    update app.js

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Pages/Home'
+import Statusbar from './Layout/Statusbar';
 function App() {
   return (
    <>
+   <Statusbar></Statusbar>
    <Home></Home>
    </>
   );
