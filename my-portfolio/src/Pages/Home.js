745e4eaba0f2f806b6443e4f202a1bfdb9025179
Your Name <lokeshtiwari212@gmail.com>
2024-03-21 15:03:19 +0530

    updated hpme

--- my-portfolio/src/Pages/Home.js
+++ my-portfolio/src/Pages/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Statusbar from "../Layout/Statusbar";
 import Hero from "../Components/Hero";
 import Projects from "../Components/Projects";
 import Experience from "../Components/Experience";
@@ -10,7 +9,6 @@ import Skills from "../Components/Skills";
 function Home() {
   return (
     <>
-      <Statusbar />
       <Hero />
       <Skills/>
       <Experience/>
