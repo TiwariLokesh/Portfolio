53a97f6b8db6d1a1d2953886c6c4028996f9368b
Your Name <lokeshtiwari212@gmail.com>
2024-03-22 18:24:56 +0530

    starting contactheader page

--- my-portfolio/src/Components/ContactHeader.js
+++ my-portfolio/src/Components/ContactHeader.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { Container,Row } from 'react-bootstrap'
+import { Container,Row, Col } from 'react-bootstrap'
 import '../Stylings/Contact.css'
 function ContactHeader() {
   return (
    <Container fluid>
 <Row className="header">
+<Col md={6}>
 
+</Col>
+
+<Col md={6}>
+
+</Col>
 </Row>
    </Container>
   )
