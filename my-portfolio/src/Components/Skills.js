6ff6145618f10eeebb019f33c7270e9b329630dd
Your Name <lokeshtiwari212@gmail.com>
2024-03-22 11:49:09 +0530

    made a crousel for skills

--- my-portfolio/src/Components/Skills.js
+++ my-portfolio/src/Components/Skills.js
@@ -6,8 +6,18 @@ import css from '../Images/css.png';
 import js from '../Images/js.png';
 import react from '../Images/react.png';
 import Button from "react-bootstrap/Button";
-
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 function Skills() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 4,
+    autoplay: true,
+    slidesToScroll: 1
+  };
   return (
     <Container className="pt-5 pb-5">
       <Row>
@@ -20,30 +30,33 @@ function Skills() {
       </Row>
 
       <Row>
-        <Col lg={3} className="p-4">
+        <Slider {...settings}>
+
+          <Col lg={3} className="p-4">
+            <div className="skills-div d-flex flex-column justify-content-center align-items-center">
+              <img src={html}></img>
+              <p className="my-2">HTML</p>
+            </div>
+          </Col>
+          <Col lg={3} className="p-4">
           <div className="skills-div d-flex flex-column justify-content-center align-items-center">
-            <img src={html}></img>
-            <p className="my-2">HTML</p>
-          </div>
-        </Col>
-        <Col lg={3} className="p-4">
-        <div className="skills-div d-flex flex-column justify-content-center align-items-center">
-            <img src={css}></img>
-            <p className="my-2">CSS</p>
-          </div>
-        </Col>
-        <Col lg={3} className="p-4">
-        <div className="skills-div d-flex flex-column justify-content-center align-items-center">
-            <img src={js}></img>
-            <p className="my-2">JAVASCRIPT</p>
-          </div>
-        </Col>
-        <Col lg={3} className="p-4">
-        <div className="skills-div d-flex flex-column justify-content-center align-items-center ">
-            <img src={react}></img>
-            <p className="my-2">REACT</p>
-          </div>
-        </Col>
+              <img src={css}></img>
+              <p className="my-2">CSS</p>
+            </div>
+          </Col>
+          <Col lg={3} className="p-4">
+          <div className="skills-div d-flex flex-column justify-content-center align-items-center">
+              <img src={js}></img>
+              <p className="my-2">JAVASCRIPT</p>
+            </div>
+          </Col>
+          <Col lg={3} className="p-4">
+          <div className="skills-div d-flex flex-column justify-content-center align-items-center ">
+              <img src={react}></img>
+              <p className="my-2">REACT</p>
+            </div>
+          </Col>
+        </Slider>
       </Row>
 
       <Row className="pb-5 pt-5"> 
