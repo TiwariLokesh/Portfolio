a91192163e9d554669dfe1f20660c7091c927e67
Your Name <lokeshtiwari212@gmail.com>
2024-03-26 16:50:43 +0530

    updated ui

--- my-portfolio/src/Components/MyInformation.js
+++ my-portfolio/src/Components/MyInformation.js
@@ -8,9 +8,9 @@ function MyInformation() {
       <Container>
         <Row className="align-items-center  justify-content-around">
           <div className="d-flex justify-content-center align-items-center text-center">
-          <Col md={4}><i className=" font-icon fa-solid fa-location-dot"></i><h3>My Address</h3><p>Chandigarh, India</p></Col>
-          <Col md={4}><i className=" font-icon fa-solid fa-envelope"></i><h3>Send me message</h3><p>lokeshtiwari212@gmail.com</p></Col>
-          <Col md={4}><i className=" font-icon fa-solid fa-phone"></i><h3>Call me on</h3><p>+91 7248475238</p></Col>
+          <Col md={4}><i className=" font-icon fa-solid fa-location-dot"></i><h3 className="pt-3">My Address</h3><p>Chandigarh, India</p></Col>
+          <Col md={4}><i className=" font-icon fa-solid fa-envelope"></i><h3 className="pt-3">Send me message</h3><p>lokeshtiwari212@gmail.com</p></Col>
+          <Col md={4}><i className=" font-icon fa-solid fa-phone"></i><h3 className="pt-3">Call me on</h3><p>+91 7248475238</p></Col>
           </div>
         </Row>
       </Container>
