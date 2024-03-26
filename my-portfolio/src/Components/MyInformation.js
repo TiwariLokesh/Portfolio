9c4649d3fad3e1b765a2c97d4105d316fd788048
Your Name <lokeshtiwari212@gmail.com>
2024-03-26 17:56:12 +0530

    improved information section

--- my-portfolio/src/Components/MyInformation.js
+++ my-portfolio/src/Components/MyInformation.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
-
 function MyInformation() {
   return (
-    <div className="information-part p-4 ">
-      <Container>
-        <Row className="align-items-center  justify-content-around">
-          <div className="d-flex justify-content-center align-items-center text-center">
-          <Col md={4}><i className=" font-icon fa-solid fa-location-dot"></i><h3 className="pt-3">My Address</h3><p>Chandigarh, India</p></Col>
-          <Col md={4}><i className=" font-icon fa-solid fa-envelope"></i><h3 className="pt-3">Send me message</h3><p>lokeshtiwari212@gmail.com</p></Col>
-          <Col md={4}><i className=" font-icon fa-solid fa-phone"></i><h3 className="pt-3">Call me on</h3><p>+91 7248475238</p></Col>
-          </div>
+    <div className="information-part p-4">
+      <Container className="mx-auto">
+        <Row className=" justify-content-center">
+          <Col md={4}>
+            <i className=" font-icon fa-solid fa-location-dot mx-auto text-center"></i>
+            <h3 className="pt-3 mx-auto text-center">My Address</h3>
+            <p className="mx-auto text-center">Chandigarh, India</p>
+          </Col>
+          <Col md={4}>
+            <i className=" font-icon fa-solid fa-envelope mx-auto"></i>
+            <h3 className="pt-3 mx-auto text-center">Send me message</h3>
+            <p className="mx-auto text-center"> lokeshtiwari212@gmail.com</p>
+          </Col>
+          <Col md={4}>
+            <i className=" font-icon fa-solid fa-phone mx-auto"></i>
+            <h3 className="pt-3 text-center">Call me on</h3>
+            <p className="mx-auto text-center">+91 7248475238</p>
+          </Col>
         </Row>
       </Container>
     </div>
