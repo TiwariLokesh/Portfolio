3e268f900fc9de9c06e847a61c6b4c22da88690f
Your Name <lokeshtiwari212@gmail.com>
2024-03-26 16:24:37 +0530

    updated next part

--- my-portfolio/src/Components/MyInformation.js
+++ my-portfolio/src/Components/MyInformation.js
@@ -4,10 +4,14 @@ import { Container, Row, Col } from "react-bootstrap";
 
 function MyInformation() {
   return (
-    <div>
+    <div className="information-part p-4 ">
       <Container>
-        <Row>
-          <Col md={4}><i class="fa-solid fa-location-dot"></i></Col>
+        <Row className="align-items-center  justify-content-around">
+          <div className="d-flex justify-content-center align-items-center text-center">
+          <Col md={4}><i class=" font-icon fa-solid fa-location-dot"></i><h3>My Address</h3><p>Chandigarh, India</p></Col>
+          <Col md={4}><i class=" font-icon fa-solid fa-envelope"></i><h3>Send me message</h3><p>lokeshtiwari212@gmail.com</p></Col>
+          <Col md={4}><i class=" font-icon fa-solid fa-phone"></i><h3>Call me on</h3><p>+91 7248475238</p></Col>
+          </div>
         </Row>
       </Container>
     </div>
