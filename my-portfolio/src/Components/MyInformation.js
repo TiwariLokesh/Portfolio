87d170dc0d0ee3a5ad72ab54ae992438a3a461cc
Your Name <lokeshtiwari212@gmail.com>
2024-03-27 14:45:34 +0530

    updated

--- my-portfolio/src/Components/MyInformation.js
+++ my-portfolio/src/Components/MyInformation.js
@@ -5,21 +5,21 @@ function MyInformation() {
   return (
     <div className="information-part p-4">
       <Container className="mx-auto">
-        <Row className=" justify-content-center">
+        <Row>
           <Col md={4}>
             <i className=" font-icon fa-solid fa-location-dot mx-auto text-center"></i>
-            <h3 className="pt-3 mx-auto text-center">My Address</h3>
-            <p className="mx-auto text-center">Chandigarh, India</p>
+            <h3 className="pt-3 text-center">My Address</h3>
+            <p className=" text-center data">Chandigarh, India</p>
           </Col>
           <Col md={4}>
             <i className=" font-icon fa-solid fa-envelope mx-auto"></i>
-            <h3 className="pt-3 mx-auto text-center">Send me message</h3>
-            <p className="mx-auto text-center"> lokeshtiwari212@gmail.com</p>
+            <h3 className="pt-3 text-center">Send me message</h3>
+            <p className=" text-center data"> lokeshtiwari212@gmail.com</p>
           </Col>
           <Col md={4}>
             <i className=" font-icon fa-solid fa-phone mx-auto"></i>
             <h3 className="pt-3 text-center">Call me on</h3>
-            <p className="mx-auto text-center">+91 7248475238</p>
+          <p className="text-center data">+91 7248475238</p>
           </Col>
         </Row>
       </Container>
