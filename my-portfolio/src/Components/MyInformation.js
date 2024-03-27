59b9dcae653c73fc64cc977c60c545fbf44dcc68
Your Name <lokeshtiwari212@gmail.com>
2024-03-27 14:50:13 +0530

    added media query

--- my-portfolio/src/Components/MyInformation.js
+++ my-portfolio/src/Components/MyInformation.js
@@ -6,17 +6,17 @@ function MyInformation() {
     <div className="information-part p-4">
       <Container className="mx-auto">
         <Row>
-          <Col md={4}>
+          <Col md={4} className="contact-col">
             <i className=" font-icon fa-solid fa-location-dot mx-auto text-center"></i>
             <h3 className="pt-3 text-center">My Address</h3>
             <p className=" text-center data">Chandigarh, India</p>
           </Col>
-          <Col md={4}>
+          <Col md={4} className="contact-col">
             <i className=" font-icon fa-solid fa-envelope mx-auto"></i>
             <h3 className="pt-3 text-center">Send me message</h3>
             <p className=" text-center data"> lokeshtiwari212@gmail.com</p>
           </Col>
-          <Col md={4}>
+          <Col md={4} className="contact-col">
             <i className=" font-icon fa-solid fa-phone mx-auto"></i>
             <h3 className="pt-3 text-center">Call me on</h3>
           <p className="text-center data">+91 7248475238</p>
