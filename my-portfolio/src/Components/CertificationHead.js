40dca0f03cb948244ec1c1d5657e1fd60795f5a4
Your Name <lokeshtiwari212@gmail.com>
2024-03-29 18:25:09 +0530

    adding css

--- my-portfolio/src/Components/CertificationHead.js
+++ my-portfolio/src/Components/CertificationHead.js
@@ -6,10 +6,14 @@ function CertificationHead() {
     <>
       <Container>
         <Row>
-          <Col md={6}>
+          <Col md={6} className="certificate-para">
           <div className="pt-5 project-heading">
               My <span>Skills</span>
             </div>
+            <p className="text-justify">My certifications represent dedication and expertise in my field, strengthening my skills with each earned credential.</p>
+            <p className="text-justify">They stand as a testament to my commitment to excellence, validating my readiness to tackle diverse challenges.</p>
+            <p className="text-justify">These certifications signify trust in my abilities, serving as a foundation for professional growth.</p>
+            <p className="text-justify">Continual learning keeps me abreast of industry advancements, ensuring innovative solutions and exceeding expectations.</p>
           </Col>
 
           <Col md={6}>
