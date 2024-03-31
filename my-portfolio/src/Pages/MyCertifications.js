9e1495f856b325eddbc37f302200a44f96dd9800
Your Name <lokeshtiwari212@gmail.com>
2024-03-31 16:31:00 +0530

    formatted

--- my-portfolio/src/Pages/MyCertifications.js
+++ my-portfolio/src/Pages/MyCertifications.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import CertificationHead from '../Components/CertificationHead';
-import CertificateCard from '../Components/CertificateCard';
-import CertificateData from '../Components/CertificateData';
+import React from "react";
+import CertificationHead from "../Components/CertificationHead";
+import CertificateCard from "../Components/CertificateCard";
+import CertificateData from "../Components/CertificateData";
 
 function MyCertifications() {
   return (
@@ -9,15 +9,20 @@ function MyCertifications() {
       <div>
         <CertificationHead />
       </div>
+
       <div>
         <p className="project-para pb-5 fs-2 pt-5">
-          <span className='text-muted project-para fs-2'>Delve into</span> <br/>My Certified Proficiency
+          <span className="text-muted project-para fs-2">Delve into</span>{" "}
+          <br />
+          My Certified Proficiency
         </p>
 
         {CertificateData.map((row, index) => (
           <CertificateCard key={index} data={row} />
         ))}
       </div>
+
+      
     </>
   );
 }
