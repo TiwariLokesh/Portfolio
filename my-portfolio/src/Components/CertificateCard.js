461d0ab25583d34b61ad9f1bfb9c0004173d1d06
Your Name <lokeshtiwari212@gmail.com>
2024-03-31 13:11:16 +0530

    added more styling in certificates card

--- my-portfolio/src/Components/CertificateCard.js
+++ my-portfolio/src/Components/CertificateCard.js
@@ -6,11 +6,13 @@ const CertificateCard = () => {
     <CDBContainer className='pt-5'>
       <CDBCard style={{ width: '25rem',borderRadius:"20px",borderColor:"#ca0018" }} border  className='p-3'>
         <CDBCardBody>
-          <h5>Heading</h5>
+        <h3 className="heading">
+                <span>WEB DEVELOPMENT</span>
+              </h3>
           <p>Paragraph</p>
-          <div className="d-flex justify-content-center">
-            <CDBBtn color="light" flat circle>
-              Button
+          <div className="d-flex justify-content-start">
+            <CDBBtn  style={{color:"#ca0018",backgroundColor:"transparent", border:"1px solid #ca0018",borderRadius:"50px"}} flat >
+              See Certificate
             </CDBBtn>
           </div>
         </CDBCardBody>
