39226237cecb9325bb26a8348352d83eead441bf
Your Name <lokeshtiwari212@gmail.com>
2024-03-31 12:17:28 +0530

    added fontawesome icons

--- my-portfolio/src/Components/CertificationHead.js
+++ my-portfolio/src/Components/CertificationHead.js
@@ -11,10 +11,10 @@ function CertificationHead() {
           <div className="pt-5 project-heading ">
               My <span>CERTIFICATIONS</span>
             </div>
-            <p className="text-justify">My certifications represent dedication and expertise in my field, strengthening my skills with each earned credential.</p>
-            <p className="text-justify">They stand as a testament to my commitment to excellence, validating my readiness to tackle diverse challenges.</p>
-            <p className="text-justify">These certifications signify trust in my abilities, serving as a foundation for professional growth.</p>
-            <p className="text-justify">Continual learning keeps me abreast of industry advancements, ensuring innovative solutions and exceeding expectations.</p>
+             <p className="text-justify"><i class="fa-solid fa-greater-than" style={{color: "#ca0018"}}></i> My certifications represent dedication and expertise in my field, strengthening my skills with each earned credential.</p>
+            <p className="text-justify"><i class="fa-solid fa-greater-than" style={{color: "#ca0018"}}></i> They stand as a testament to my commitment to excellence, validating my readiness to tackle diverse challenges.</p>
+            <p className="text-justify"><i class="fa-solid fa-greater-than" style={{color: "#ca0018"}}></i> These certifications signify trust in my abilities, serving as a foundation for professional growth.</p>
+            <p className="text-justify"><i class="fa-solid fa-greater-than" style={{color: "#ca0018"}}></i> Continual learning keeps me abreast of industry advancements, ensuring innovative solutions and exceeding expectations.</p>
           </Col>
 
           <Col md={6} className="certificate-img">
