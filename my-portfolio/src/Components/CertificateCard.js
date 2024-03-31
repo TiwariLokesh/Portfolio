907e96105145117524883951f722fcf8214da3c9
Your Name <lokeshtiwari212@gmail.com>
2024-03-31 12:48:26 +0530

    added card for certificated

---
+++ my-portfolio/src/Components/CertificateCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { CDBCard, CDBCardBody, CDBBtn, CDBContainer } from 'cdbreact';
+
+const CertificateCard = () => {
+  return (
+    <CDBContainer>
+      <CDBCard style={{ width: '25rem',borderRadius:"20px" }} border >
+        <CDBCardBody>
+          <h5>Heading</h5>
+          <p>Paragraph</p>
+          <div className="d-flex justify-content-center">
+            <CDBBtn color="light" flat circle>
+              Button
+            </CDBBtn>
+          </div>
+        </CDBCardBody>
+      </CDBCard>
+    </CDBContainer>
+  );
+};
+
+export default CertificateCard;
--- my-portfolio/src/Pages/MyCertifications.js
+++ my-portfolio/src/Pages/MyCertifications.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import CertificationHead from '../Components/CertificationHead'
-
+import CertificateCard from '../Components/CertificateCard'
 
 function MyCertifications() {
   return (
     <div>
   <CertificationHead/>
+  <CertificateCard/>
     </div>
   )
 }
