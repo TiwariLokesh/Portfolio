ad924e3842a5f96d461fdd559d13ae5d97aabd14
Your Name <lokeshtiwari212@gmail.com>
2024-04-02 11:47:43 +0530

    made footer file

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Statusbar from './Layout/Statusbar';
+import Footer from './Layout/Footer';
 function App() {
   return (
    <>
    <Statusbar></Statusbar>
-   
+   <Footer></Footer>
    </>
   );
 }
