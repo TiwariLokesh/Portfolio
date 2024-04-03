e6277da0f51132a74aee1659c01ff337c887be56
Your Name <lokeshtiwari212@gmail.com>
2024-04-03 23:25:04 +0530

    changed info

--- my-portfolio/src/Components/Service.js
+++ my-portfolio/src/Components/Service.js
@@ -64,8 +64,7 @@ const Service = () => {
                     <p>
                       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam
                       minima, soluta doloribus reiciendis molestiae placeat unde eos molestias.
-                      Quisquam aperiam, pariatur. Tempora, placeat ratione porro voluptate odit
-                      minima.
+                      
                     </p>
                   </Tab.Pane>
                   <Tab.Pane eventKey="2">
