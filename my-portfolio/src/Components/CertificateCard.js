202f8f7430ff77ed3993453b4f772183dee3aa1e
Your Name <lokeshtiwari212@gmail.com>
2024-04-30 21:46:30 +0530

    added bootstrap card

--- my-portfolio/src/Components/CertificateCard.js
+++ my-portfolio/src/Components/CertificateCard.js
@@ -47,3 +47,29 @@
 // };
 
 // export default CertificateCard;
+
+
+
+
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function BasicExample() {
+  return (
+    <div className='pb-5 '>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+    </div>
+  );
+}
+
+export default BasicExample;
