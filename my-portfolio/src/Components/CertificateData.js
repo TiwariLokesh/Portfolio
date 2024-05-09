4d8e7e90d15608ca9e4661988735eabfb232cb80
Your Name <lokeshtiwari212@gmail.com>
2024-05-09 19:08:21 +0530

    imported all photos for cards

--- my-portfolio/src/Components/CertificateData.js
+++ my-portfolio/src/Components/CertificateData.js
@@ -1,4 +1,9 @@
 import internshipImg from '../Images/Certificate/internship.png'
+import web from '../Images/Certificate/web.png'
+import graphic from '../Images/Certificate/graphic.png'
+import cloud from '../Images/Certificate/cloud.png'
+import workshop from '../Images/Certificate/workshop.png'
+import hackathon from '../Images/Certificate/hackathon.png'
 
 const CertificateData = [
   {
@@ -9,35 +14,27 @@ const CertificateData = [
   {
     heading: "WEB DEVELOPMENT",
     disc: "Certified in Web Development encompassing HTML, CSS, JavaScript, jQuery, Bootstrap, and WordPress. Proficient in crafting dynamic and responsive websites with a focus on user experience.",
-    img: "https://img.freepik.com/free-vector/web-development-concept-with-programmer-ar_107791-17049.jpg"
+    img: web
   },
   {
     heading: "GRAPHIC DESIGNING",
     disc: "Professionally certified in Graphic Designing, adept in Adobe Photoshop and Canva, specializing in creative design solutions, brand identity development, and compelling visual storytelling techniques.",
-    img: "https://img.freepik.com/free-vector/postponed-concept_23-2148508247.jpg?w=1060&t=st=1714667577~exp=1714668177~hmac=cd3f0b27653f5a6fb7cc6daf25fde7ab4d34a176553bf740ecef131fb10d2877"
+    img: graphic
   },
   {
     heading: "GOOGLE CLOUD",
     disc: "Certified in Google Cloud Computing Foundations & Generative AI, equipped with essential skills for navigating Google Cloud. Prepared to excel in cloud computing with a deep understanding of core concepts.",
-    img: "https://img.freepik.com/free-vector/technology-blue_24877-49447.jpg?t=st=1714667756~exp=1714671356~hmac=0717cc40e70a4fb32326d60e8c26aee6beac7ee069b994d608ad7c1864a186bf&w=826"
+    img: cloud
   },
   {
     heading: "WORKSHOP",
     disc: "Conducted a workshop on web development and graphic design at Garden Valley Public School with support from Techlearns Academy. Excited to inspire and educate future tech and design enthusiasts.",
-    img : "https://img.freepik.com/free-vector/seminar-concept-illustration_114360-7480.jpg?t=st=1714667887~exp=1714671487~hmac=88b51e37acd7c8caf1953d9ed5e7ed1e1d654b782b838ac002150015602d81a3&w=1060"
+    img : workshop
   },
   {
     heading: "HACKATHON",
     disc: "Conducted a workshop on web development and graphic design at Garden Valley Public School with support from Techlearns Academy. Excited to inspire and educate future tech and design enthusiasts.",
-    img : "https://img.freepik.com/free-vector/hackathon-doodle-hand-drawing-team-programmers-web-developers-managers-graphic-designers-deve_88138-1348.jpg?t=st=1714667947~exp=1714671547~hmac=fcb104fb18958a77931396a851fd203f10cd4b70036bfe786ce3677bd2a12f16&w=1060"
-  },
-  {
-    heading: "HACKATHON VOLUNTEER",
-    disc: "Conducted a workshop on web development and graphic design at Garden Valley Public School with support from Techlearns Academy. Excited to inspire and educate future tech and design enthusiasts."
-  },
-  {
-    heading: "PYTHON",
-    disc: "Conducted a workshop on web development and graphic design at Garden Valley Public School with support from Techlearns Academy. Excited to inspire and educate future tech and design enthusiasts."
+    img : hackathon
   },
 ];
 
