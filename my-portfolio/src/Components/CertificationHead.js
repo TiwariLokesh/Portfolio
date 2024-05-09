2a0f53ad62b20f42da7433d879db78c53a4dfbac
Your Name <lokeshtiwari212@gmail.com>
2024-05-09 19:41:42 +0530

    set media query

--- my-portfolio/src/Components/CertificationHead.js
+++ my-portfolio/src/Components/CertificationHead.js
@@ -2,28 +2,39 @@ import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import '../Stylings/Certifications.css'
 import Certification from '../Images/Certification.png'
+
+
 function CertificationHead() {
   return (
     <>
       <Container>
         <Row>
-          <Col md={6} className="certificate-para pt-5 pb-5">
-          <div className="pt-5 project-heading ">
+          <Col md={6} className="certificate-para pt-5 pb-5" style={{marginTop:"50px"}}>
+            <div className="pt-5 project-heading p-1 cert">
               My <span>CERTIFICATIONS</span>
             </div>
-             <p className="text-justify"><i class="fa-solid fa-greater-than" style={{color: "#ca0018"}}></i> My certifications represent dedication and expertise in my field, strengthening my skills with each earned credential.</p>
-            <p className="text-justify"><i class="fa-solid fa-greater-than" style={{color: "#ca0018"}}></i> They stand as a testament to my commitment to excellence, validating my readiness to tackle diverse challenges.</p>
-            <p className="text-justify"><i class="fa-solid fa-greater-than" style={{color: "#ca0018"}}></i> These certifications signify trust in my abilities, serving as a foundation for professional growth.</p>
-            <p className="text-justify"><i class="fa-solid fa-greater-than" style={{color: "#ca0018"}}></i> Continual learning keeps me abreast of industry advancements, ensuring innovative solutions and exceeding expectations.</p>
+            <p className="text-justify">
+              <i className="fa-solid fa-greater-than p-2 pt-4" style={{ color: "#ca0018" }}></i> My certifications represent dedication and expertise in my field, strengthening my skills with each earned credential.
+            </p>
+            <p className="text-justify">
+              <i className="fa-solid fa-greater-than p-2 pt-2" style={{ color: "#ca0018" }}></i> They stand as a testament to my commitment to excellence, validating my readiness to tackle diverse challenges.
+            </p>
+            <p className="text-justify">
+              <i className="fa-solid fa-greater-than p-2 pt-2" style={{ color: "#ca0018" }}></i> These certifications signify trust in my abilities, serving as a foundation for professional growth.
+            </p>
+            <p className="text-justify">
+              <i className="fa-solid fa-greater-than p-2 pt-2" style={{ color: "#ca0018" }}></i> Continual learning keeps me abreast of industry advancements, ensuring innovative solutions and exceeding expectations.
+            </p>
           </Col>
 
-          <Col md={6} className="certificate-img">
-          <img style={{width:"100%"}} src={Certification}></img>
+          <Col md={6} className="certificate-img d-flex align-items-center justify-content-center">
+            <img src={Certification} style={{ maxWidth: "100%", height: "auto" }} alt="Certification" />
           </Col>
         </Row>
       </Container>
     </>
   );
 }
+
 
 export default CertificationHead;
