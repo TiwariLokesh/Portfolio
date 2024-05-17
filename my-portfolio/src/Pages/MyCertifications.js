fae8b31df59cb614953fd08668c8865cb52a7c71
Your Name <lokeshtiwari212@gmail.com>
2024-05-17 18:34:53 +0530

    added image

--- my-portfolio/src/Pages/MyCertifications.js
+++ my-portfolio/src/Pages/MyCertifications.js
@@ -34,7 +34,7 @@ function MyCertifications() {
               <p className="project-para pb-5 fs-2 pt-5">
                 <span className="text-muted project-para fs-2">Delve into</span>{" "}
                 <br />
-                My Certified Proficiency
+                My Honors & Awards
               </p>
             </div>
             {AwardsData.map((row, index) => (
