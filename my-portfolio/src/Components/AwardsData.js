ef2ea9575010c852004b5ec07331acab58d1b55d
Your Name <lokeshtiwari212@gmail.com>
2024-05-17 18:49:11 +0530

    imported some images

--- my-portfolio/src/Components/AwardsData.js
+++ my-portfolio/src/Components/AwardsData.js
@@ -1,5 +1,5 @@
 import ncc from '../Images/Certificate/ncc.png'
-
+import atc from '../Images/Certificate/atc.png'
 const AwardsData = [
     {
         heading:"NCC Navy",
@@ -8,9 +8,9 @@ const AwardsData = [
         certficateImg:"https://img.freepik.com/free-photo/top-view-economy-concept_23-2148547883.jpg?t=st=1715949796~exp=1715953396~hmac=593ee5dad0ef5e99184b0367a73c5c16c435d5d032b3af5cd71a5ebd0b266f62&w=1060"
       },
       {
-        heading:"NCC Navy",
+        heading:"Annual Training Camp",
         disc:"I proudly earned the NCC Navy 'A' certificate, demonstrating discipline, leadership, and dedication through rigorous training and naval activities, preparing me to contribute effectively in challenging environments.",
-        img : ncc,
+        img : atc,
         certficateImg:"https://img.freepik.com/free-photo/top-view-economy-concept_23-2148547883.jpg?t=st=1715949796~exp=1715953396~hmac=593ee5dad0ef5e99184b0367a73c5c16c435d5d032b3af5cd71a5ebd0b266f62&w=1060"
       }
 ]
