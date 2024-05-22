8acdb56d99fb3a0f4ef224950bf1a37fc7aedbac
Your Name <lokeshtiwari212@gmail.com>
2024-05-22 21:42:38 +0530

    changed photos.js

--- my-portfolio/src/Components/photos.js
+++ my-portfolio/src/Components/photos.js
@@ -1,39 +1,47 @@
 export const photos = [
-    {
-      src: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
-      orientation: "portrait",
-    },
-    {
-      src: "https://pixlr.com/images/index/ai-image-generator-one.webp",
-      orientation: "landscape",
-    },
-    {
-      src: "https://source.unsplash.com/qDkso9nvCg0/600x900",
-      orientation: "portrait",
-    },
-    {
-      src: "https://source.unsplash.com/iecJiKe_RNg/900x600",
-      orientation: "landscape",
-    },
-    {
-      src: "https://source.unsplash.com/epcsn8Ed8kY/900x1600",
-      orientation: "portrait",
-    },
-    {
-      src: "https://source.unsplash.com/NQSWvyVRIJk/1200x800",
-      orientation: "landscape",
-    },
-    {
-      src: "https://source.unsplash.com/zh7GEuORbUw/900x600",
-      orientation: "landscape",
-    },
-    {
-      src: "https://source.unsplash.com/PpOHJezOalU/1600x900",
-      orientation: "landscape",
-    },
-    {
-      src: "https://source.unsplash.com/I1ASdgphUH4/800x1200",
-      orientation: "portrait",
-    },
-  ];
-  
+  {
+    src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+    width: 1,
+    height: 1
+  },
+  {
+    src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    width: 4,
+    height: 3
+  }
+];
