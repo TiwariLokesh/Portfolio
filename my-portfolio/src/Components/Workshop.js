516e7405b2c7b021f7e8a7789707daefc31e8618
Your Name <lokeshtiwari212@gmail.com>
2024-05-23 21:29:17 +0530

    adding a photo gallery

--- my-portfolio/src/Components/Workshop.js
+++ my-portfolio/src/Components/Workshop.js
@@ -1,45 +1,88 @@
-import React, { useState, useCallback } from "react";
-import ReactDOM from "react-dom";
-import { photos } from '../Components/photos';
+// src/components/PhotoGallery.js
+import React, { useState } from 'react';
 import '../Stylings/Workshop.css';
 
-const Workshop = () => {
-  const [currentImage, setCurrentImage] = useState(0);
-  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+const photos = [
+  {
+    src: 'https://source.unsplash.com/2ShvY8Lf6l0/800x599',
+    width: 4,
+    height: 3.5
+  },
+  {
+    src: 'https://source.unsplash.com/Dm-qxdynoEc/800x799',
+    width: 2.5,
+    height: 3.5
+  },
+  {
+    src: 'https://source.unsplash.com/qDkso9nvCg0/600x799',
+    width: 3,
+    height: 3.5
+  },
+  {
+    src: 'https://source.unsplash.com/iecJiKe_RNg/600x799',
+    width: 3,
+    height: 3.5
+  },
+  {
+    src: 'https://source.unsplash.com/epcsn8Ed8kY/600x799',
+    width: 3,
+    height: 3
+  },
+  {
+    src: 'https://source.unsplash.com/NQSWvyVRIJk/800x599',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://source.unsplash.com/zh7GEuORbUw/600x799',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://source.unsplash.com/PpOHJezOalU/800x599',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://source.unsplash.com/I1ASdgphUH4/800x599',
+    width: 4,
+    height: 3
+  }
+];
 
-  const openLightbox = useCallback((index) => {
+const Workshop = () => {
+  const [currentImage, setCurrentImage] = useState(null);
+
+  const openLightbox = (index) => {
     setCurrentImage(index);
-    setViewerIsOpen(true);
-  }, []);
+  };
 
   const closeLightbox = () => {
-    setCurrentImage(0);
-    setViewerIsOpen(false);
+    setCurrentImage(null);
   };
 
   return (
-    <div>
+    <div className="gallery-container">
       <div className="gallery">
         {photos.map((photo, index) => (
           <img
             key={index}
             src={photo.src}
-            alt={`Photo ${index + 1}`}
-            className="gallery-image"
+            alt={`Photo ${index}`}
+            style={{ width: `${photo.width * 100}px`, height: `${photo.height * 100}px` }}
             onClick={() => openLightbox(index)}
           />
         ))}
       </div>
-      {viewerIsOpen && (
-        <div className="modal" onClick={closeLightbox}>
-          <span className="close" onClick={closeLightbox}>&times;</span>
-          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-            <img src={photos[currentImage].src} alt={`Photo ${currentImage + 1}`} />
-          </div>
+
+      {currentImage !== null && (
+        <div className="lightbox" onClick={closeLightbox}>
+          <span className="close">&times;</span>
+          <img className="lightbox-content" src={photos[currentImage].src} alt={`Photo ${currentImage}`} />
         </div>
       )}
     </div>
   );
-}
+};
 
 export default Workshop;
