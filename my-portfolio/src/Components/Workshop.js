9bc893822e23f1f351df30b9e09576ba6854e086
Your Name <lokeshtiwari212@gmail.com>
2024-06-21 22:46:15 +0530

    improved some

--- my-portfolio/src/Components/Workshop.js
+++ my-portfolio/src/Components/Workshop.js
@@ -1,37 +1,30 @@
 // src/components/PhotoGallery.js
 import React, { useState } from 'react';
 import '../Stylings/Workshop.css';
-import image1 from '../Images/Workshop/1.png'
-import image2 from '../Images/Workshop/2.png'
-import image3 from '../Images/Workshop/3.png'
-import image4 from '../Images/Workshop/4.png'
-
-
+import image1 from '../Images/Workshop/1.png';
+import image2 from '../Images/Workshop/2.png';
+import image3 from '../Images/Workshop/3.png';
+import image4 from '../Images/Workshop/4.png';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const photos = [
-  // https://images.unsplash.com/photo-1561409037-5d7c064238bd?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
   {
-    // src: 'https://source.unsplash.com/2ShvY8Lf6l0/800x599',
-    src : image1,
-    width: 3.5,
+    src: image1,
+    width: 4.5,
     height: 2.5
   },
-  
-  
   {
-    src : image2,
+    src: image2,
     width: 3,
     height: 2.5
   },
-  
   {
-    src : image3,
+    src: image3,
     width: 4,
     height: 2.5
   },
-  
   {
-    src : image4,
+    src: image4,
     width: 3.3,
     height: 2.5
   },
@@ -74,26 +67,36 @@ const Workshop = () => {
   };
 
   return (
-    <div className="gallery-container">
-      <div className="gallery">
+    <Container fluid>
+      <Row className="gallery-container">
         {photos.map((photo, index) => (
-          <img
-            key={index}
-            src={photo.src}
-            alt={`Photo ${index}`}
-            style={{ width: `${photo.width * 100}px`, height: `${photo.height * 100}px` }}
-            onClick={() => openLightbox(index)}
-          />
+          <Col xs={12} sm={6} md={4} lg={3} key={index} className="mb-4">
+            <div style={{ paddingBottom: '100%', position: 'relative' }}>
+              <img
+                src={photo.src}
+                alt={`Photo ${index}`}
+                style={{
+                  width: '100%',
+                  height: 'auto',
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  objectFit: 'cover'
+                }}
+                onClick={() => openLightbox(index)}
+              />
+            </div>
+          </Col>
         ))}
-      </div>
+      </Row>
 
       {currentImage !== null && (
-        <div className="lightbox" onClick={closeLightbox} style={{zIndex:99999}}>
+        <div className="lightbox" onClick={closeLightbox} style={{ zIndex: 99999 }}>
           <span className="close">&times;</span>
           <img className="lightbox-content" src={photos[currentImage].src} alt={`Photo ${currentImage}`} />
         </div>
       )}
-    </div>
+    </Container>
   );
 };
 
